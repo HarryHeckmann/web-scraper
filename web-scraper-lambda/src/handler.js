d5e08b173b9168dc70d9ca2a5ecfc5238da4a390
Ben Bays <ben.bays@projekt202.com>
2020-04-10 07:20:38 -0500

    Define addSearch method

--- web-scraper-lambda/src/handler.js
+++ web-scraper-lambda/src/handler.js
@@ -1,14 +1,23 @@
-export async function getSearches(event) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
+export const getSearches = async event => ({
+  statusCode: 200,
+  body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
         input: event,
       },
       null,
       2
-    ),
-  };
+  ),
+});
 
-}
+export const addSearch = async event => ({
+  statusCode: 201,
+  body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+  ),
+});
